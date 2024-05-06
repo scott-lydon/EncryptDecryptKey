f42ce7240ab8cea5bcb045fe1aab31d2080514eb
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-06 02:41:38 -0300

    Add can import framework condition

--- Sources/EncryptDecryptKey/DataCrypto.swift
+++ Sources/EncryptDecryptKey/DataCrypto.swift
@@ -7,20 +7,42 @@
 //
 
 import Foundation
+#if canImport(CryptoKit)
 import CryptoKit
-
-@available(iOS 13.0, macOS 10.15, *)
+@available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
 public extension Data {
     /// Using the shared key defined earlier
-    func encrypt() throws -> Data {
-        try AES.GCM.seal(self, using: .theKey()).combined!
+    func encrypt<D: ContiguousBytes>(with key: D = []) throws -> Data {
+        try AES.GCM.seal(self, using: SymmetricKey(data: key)).combined!
     }
 
-    func decrypt() throws -> Data {
-        try AES.GCM.open(AES.GCM.SealedBox(combined: self), using: .theKey())
+    func decrypt<D: ContiguousBytes>(with key: D = []) throws -> Data {
+        try AES.GCM.open(AES.GCM.SealedBox(combined: self), using: SymmetricKey(data: key))
     }
 
     func decrypted<T: Codable>() throws -> T {
         try JSONDecoder().decode(T.self, from: decrypt())
     }
 }
+#elseif canImport(Vapor)
+import Vapor
+
+@available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
+public extension Data {
+    /// Using the shared key defined earlier
+    func encrypt(with key: [UInt8] = []) throws -> Data {
+        try AES.GCM.seal(self, using: SymmetricKey(data: key)).combined!
+    }
+
+    func decrypt(with key: [UInt8] = []) throws -> Data {
+        try AES.GCM.open(AES.GCM.SealedBox(combined: self), using: SymmetricKey(data: key))
+    }
+
+    func decrypted<T: Codable>() throws -> T {
+        try JSONDecoder().decode(T.self, from: decrypt())
+    }
+}
+
+#endif
+
+
